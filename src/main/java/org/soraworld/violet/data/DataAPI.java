66fc6941bbdd1a3f1e8c1a07c3b91621f1de35e5
Himmelt <master@void-3.cn>
2018-12-26 23:58:45 +0800

    update

--- src/main/java/org/soraworld/violet/data/DataAPI.java
+++ src/main/java/org/soraworld/violet/data/DataAPI.java
@@ -1,8 +1,6 @@
 package org.soraworld.violet.data;
 
 import org.soraworld.hocon.node.FileNode;
-import org.soraworld.hocon.node.Node;
-import org.soraworld.hocon.node.NodeMap;
 import org.soraworld.hocon.node.Options;
 import org.soraworld.violet.serializers.UUIDSerializer;
 
@@ -15,7 +13,7 @@ public final class DataAPI {
 
     public static final Options options = Options.build();
     private static final ConcurrentHashMap<UUID, HashMap<String, Object>> playerTempData = new ConcurrentHashMap<>();
-    private static final ConcurrentHashMap<UUID, NodeMap> playerStoreData = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<UUID, HashMap<String, Object>> playerStoreData = new ConcurrentHashMap<>();
 
     static {
         options.registerType(new UUIDSerializer());
@@ -116,23 +114,23 @@ public final class DataAPI {
     }
 
     public static boolean hasStore(UUID uuid, String key) {
-        NodeMap data = playerStoreData.get(uuid);
+        Map<String, Object> data = playerStoreData.get(uuid);
         if (data != null) return data.containsKey(key);
         return false;
     }
 
-    public static Node getStore(UUID uuid, String key, Node def) {
-        NodeMap data = playerStoreData.get(uuid);
+    public static Object getStore(UUID uuid, String key, Object def) {
+        Map<String, Object> data = playerStoreData.get(uuid);
         if (data != null) return data.getOrDefault(key, def);
         return def;
     }
 
-    public static void setStore(UUID uuid, String key, Node value) {
-        playerStoreData.computeIfAbsent(uuid, u -> new NodeMap(Options.defaults())).set(key, value);
+    public static void setStore(UUID uuid, String key, Object value) {
+        playerStoreData.computeIfAbsent(uuid, u -> new HashMap<>()).put(key, value);
     }
 
-    public static Node removeStore(UUID uuid, String key) {
-        NodeMap data = playerStoreData.get(uuid);
+    public static Object removeStore(UUID uuid, String key) {
+        Map<String, Object> data = playerStoreData.get(uuid);
         if (data != null) return data.remove(key);
         return null;
     }
@@ -142,12 +140,11 @@ public final class DataAPI {
     }
 
     public static void readStore(UUID uuid, FileNode node) {
-        playerStoreData.put(uuid, node);
+        playerStoreData.put(uuid, node.toTypeMap());
     }
 
     public static void writeStore(UUID uuid, FileNode node) {
-        node.clear();
-        NodeMap map = playerStoreData.get(uuid);
-        if (map != null) node.putAll(map);
+        Map<String, Object> map = playerStoreData.get(uuid);
+        if (map != null) node.fromTypeMap(map);
     }
 }
