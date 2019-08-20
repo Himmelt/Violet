91fe0940e8a7c7b71647be2dd9287e3abc18a9e6
Himmelt <master@void-3.cn>
2019-08-21 03:19:58 +0800

    fix sponge data listener bug

--- SpongeViolet/src/main/java/org/soraworld/violet/listener/DataListener.java
+++ SpongeViolet/src/main/java/org/soraworld/violet/listener/DataListener.java
@@ -4,12 +4,15 @@ import org.soraworld.violet.data.DataAPI;
 import org.soraworld.violet.inject.EventListener;
 import org.soraworld.violet.inject.Inject;
 import org.soraworld.violet.manager.FManager;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.entity.living.player.User;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.Order;
 import org.spongepowered.api.event.filter.IsCancelled;
 import org.spongepowered.api.event.filter.cause.First;
 import org.spongepowered.api.event.network.ClientConnectionEvent;
 import org.spongepowered.api.profile.GameProfile;
+import org.spongepowered.api.util.Tristate;
 
 import java.util.UUID;
 
@@ -25,16 +28,16 @@ public class DataListener {
         manager.asyncLoadData(uuid);
     }
 
-    @IsCancelled
+    @IsCancelled(Tristate.UNDEFINED)
     @Listener(order = Order.LAST)
-    public void onPlayerLoginLast(ClientConnectionEvent.Login event, @First GameProfile profile) {
-        UUID uuid = profile.getUniqueId();
+    public void onPlayerLoginLast(ClientConnectionEvent.Login event, @First User user) {
+        UUID uuid = user.getUniqueId();
         manager.asyncSaveData(uuid, true);
     }
 
     @Listener(order = Order.LAST)
-    public void onPlayerQuit(ClientConnectionEvent.Disconnect event, @First GameProfile profile) {
-        UUID uuid = profile.getUniqueId();
+    public void onPlayerQuit(ClientConnectionEvent.Disconnect event, @First Player player) {
+        UUID uuid = player.getUniqueId();
         DataAPI.clearTemp(uuid);
         manager.asyncSaveData(uuid, true);
     }
