843a3377f39cd6dbe345a8b7afa5290b0a6e73b3
Himmelt <master@void-3.cn>
2020-01-26 23:44:16 +0800

    update for 2.5.0

---
+++ SpigotViolet/src/api/java/org/bukkit/craftbukkit/v1_14_R1/CraftServer.java
@@ -0,0 +1,7 @@
+package org.bukkit.craftbukkit.v1_14_R1;
+
+/**
+ * @author Himmelt
+ */
+public class CraftServer {
+}
--- SpigotViolet/src/main/java/org/soraworld/violet/nms/Version.java
+++ SpigotViolet/src/main/java/org/soraworld/violet/nms/Version.java
@@ -14,10 +14,13 @@ public final class Version {
     public static final boolean v1_12_R1;
     public static final boolean v1_13_R1;
     public static final boolean v1_13_R2;
+    public static final boolean v1_14_R1;
+    public static final boolean v1_15_R1;
 
     static {
         boolean v1_7_r4 = false, v1_8_r1 = false, v1_8_r3 = false, v1_9_r1 = false, v1_9_r2 = false,
-                v1_10_r1 = false, v1_11_r1 = false, v1_12_r1 = false, v1_13_r1 = false, v1_13_r2 = false;
+                v1_10_r1 = false, v1_11_r1 = false, v1_12_r1 = false, v1_13_r1 = false, v1_13_r2 = false,
+                v1_14_r1 = false, v1_15_r1 = false;
         try {
             org.bukkit.craftbukkit.v1_7_R4.CraftServer.class.getName();
             net.minecraft.server.v1_7_R4.MinecraftServer.class.getName();
@@ -78,6 +81,18 @@ public final class Version {
             v1_13_r2 = true;
         } catch (Throwable ignored) {
         }
+        try {
+            org.bukkit.craftbukkit.v1_14_R1.CraftServer.class.getName();
+            net.minecraft.server.v1_14_R1.MinecraftServer.class.getName();
+            v1_14_r1 = true;
+        } catch (Throwable ignored) {
+        }
+        try {
+            org.bukkit.craftbukkit.v1_15_R1.CraftServer.class.getName();
+            net.minecraft.server.v1_15_R1.MinecraftServer.class.getName();
+            v1_15_r1 = true;
+        } catch (Throwable ignored) {
+        }
         v1_7_R4 = v1_7_r4;
         v1_8_R1 = v1_8_r1;
         v1_8_R3 = v1_8_r3;
@@ -88,5 +103,7 @@ public final class Version {
         v1_12_R1 = v1_12_r1;
         v1_13_R1 = v1_13_r1;
         v1_13_R2 = v1_13_r2;
+        v1_14_R1 = v1_14_r1;
+        v1_15_R1 = v1_15_r1;
     }
 }
