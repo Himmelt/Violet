ecda806fc4b6ebb5d72f19339cced34d51122236
Himmelt <master@void-3.cn>
2020-01-26 22:07:02 +0800

    update

--- SpigotViolet/src/main/java/org/soraworld/violet/manager/FManager.java
+++ SpigotViolet/src/main/java/org/soraworld/violet/manager/FManager.java
@@ -1,7 +1,6 @@
 package org.soraworld.violet.manager;
 
 import org.bukkit.command.CommandSender;
-import org.soraworld.hocon.node.Setting;
 import org.soraworld.violet.SpigotViolet;
 import org.soraworld.violet.api.IPlugin;
 import org.soraworld.violet.inject.MainManager;
@@ -9,8 +8,6 @@ import org.soraworld.violet.util.ChatColor;
 
 import java.nio.file.Path;
 import java.util.HashMap;
-import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author Himmelt
@@ -18,12 +15,7 @@ import java.util.concurrent.ConcurrentHashMap;
 @MainManager
 public final class FManager extends VManager {
 
-    @Setting(comment = "comment.uuid")
-    private UUID uuid = UUID.randomUUID();
-    private final Path dataPath;
-
     private static HashMap<String, HashMap<String, String>> langMaps = new HashMap<>();
-    private static final ConcurrentHashMap<UUID, Object> ASYNC_LOCK = new ConcurrentHashMap<>();
 
     public FManager(SpigotViolet plugin, Path path) {
         super(plugin, path);
@@ -35,7 +27,6 @@ public final class FManager extends VManager {
                 return text;
             }
         };
-        dataPath = path.resolve("storedata");
     }
 
     @Override
@@ -49,14 +40,6 @@ public final class FManager extends VManager {
     @Override
     public ChatColor defChatColor() {
         return ChatColor.DARK_PURPLE;
-    }
-
-    public UUID getUuid() {
-        if (uuid == null) {
-            uuid = UUID.randomUUID();
-            asyncSave(null);
-        }
-        return uuid;
     }
 
     public void listPlugins(CommandSender sender) {
